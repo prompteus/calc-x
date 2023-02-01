bbc5455a7b23bc009c2eec2f2eab9afe7a34af98
MartinekV <martinekvlastimil95@gmail.com>
2023-02-01 13:05:00 +0000

    Add more scenarios for chain generator

--- chain_gen.py
+++ chain_gen.py
@@ -6,8 +6,10 @@ objects_all = ['apples','pears']
 names = ['igor','vasil']
 
 scenarios = [
-    {'operation':'-', 'verb':'takes'},
-    {'operation':'+', 'verb':'gives me'},
+    {'operation':'$x-$y', 'event':'takes $y $objects from me'},
+    {'operation':'$x+$y', 'event':'gives me $y $objects'},
+    {'operation':'$x*($y+1)', 'event':'gives me $y times the amount of $objects i already have'},
+    {'operation':'$x//2', 'event':'takes half of my $objects away, rounded down'},
 ]
 
 def get_random_chain():
@@ -16,9 +18,19 @@ def get_random_chain():
     scenario = random.choice(scenarios)
     x = random.randint(0, 1000)
     y = random.randint(0, x)
-
-    prompt = f"I have {x} {objects}, {name} comes and {scenario['verb']} {y} {objects}. How many {objects} do i have?"
-    call = f"{x}{scenario['operation']}{y}"
+    
+    
+    t = Template(f"I have $x $objects, $name comes and {scenario['event']}. How many $objects do i have?")
+    prompt = t.substitute({
+        'x':x,
+        'y':y,
+        'objects':objects,
+        'name':name,
+    })
+    
+    operation = Template(scenario['operation']).substitute({'x':x,'y':y})
+    call = operation
+    
     chain = f"<python>{call}</python>"
     answer = foo(call) 
     return prompt, chain, answer
