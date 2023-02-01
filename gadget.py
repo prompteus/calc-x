2a92b1ef54b1d99cbbeb54f6c2e893ed86303ac2
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-02-01 13:29:03 +0100

    add calculator gadget

--- gadget.py
+++ gadget.py
@@ -1,16 +1,27 @@
 import abc
-
+import subprocess
 
 class Gadget(abc.ABC):
 
     def __init__(self):
         pass
 
+    @abc.abstractmethod
+    def setup(self):
+        ...
+
+    @abc.abstractmethod
     def __call__(self, input_str: str) -> str:
-        pass
+        ...
 
 
-class PythonCLI(Gadget):
+class Calculator(Gadget):
+
+    def setup(self):
+        ...
 
     def __call__(self, input_str: str) -> str:
-        return exec(input_str)
+        return subprocess.run(["python3", "-c", f"print(eval('{input_str}'))"], capture_output=True).stdout.decode("utf-8")
+
+
+foo = Calculator()
