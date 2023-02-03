cdeb0e3b63a12b23dc944a545c6dd85870b47bb7
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-02-03 23:53:42 +0100

    fix formatting of calculator gadget output

--- gadget.py
+++ gadget.py
@@ -39,9 +39,20 @@ class Calculator(Gadget):
     def __call__(self, input_str: str) -> str:
         try:
             expr = sympy.parsing.sympy_parser.parse_expr(input_str, evaluate=True)
-            string = str(expr)
-            if not isinstance(expr, sympy.core.numbers.Float):
-                string += f" ~= {expr.evalf():.6f}"
+            if isinstance(expr, sympy.core.numbers.Integer):
+                string = self.format_sympy_int(expr)
+            elif isinstance(expr, sympy.core.numbers.Float):
+                string = self.format_sympy_float(expr)
+            else:
+                string = f"{str(expr)} ~= {self.format_sympy_float(expr.evalf())}"
             return string
         except Exception as e:
             return f"ERROR: {e}"
+
+    @staticmethod
+    def format_sympy_float(x: sympy.core.numbers.Float) -> str:
+        return f"{float(x.evalf()):,.6f}".rstrip("0").rstrip(".")
+
+    @staticmethod
+    def format_sympy_int(x: sympy.core.numbers.Integer) -> str:
+        return f"{int(x):,}"
