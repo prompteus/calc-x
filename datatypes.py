b5ec18a5ec0af53488dbd41e275ecacdee47f6e9
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-02-06 00:00:55 +0100

    make datatypes easy to convert from and to json

--- datatypes.py
+++ datatypes.py
@@ -1,13 +1,45 @@
-from typing import NamedTuple
+import pydantic
 
-class Interaction(NamedTuple):
+class Interaction(pydantic.BaseModel):
+    """
+    From dict:
+    >>> dictionary = {"gadget_id": "calculator", "inputs": "2+2", "outputs": "4"}
+    >>> interaction = Interaction(**dictionary)
+
+    To json string:
+    >>> interaction.json()
+    '{"gadget_id": "calculator", "inputs": "2+2", "outputs": "4"}'
+    """
+
     gadget_id: str
     inputs: str
     outputs: str
 
-Chain = list[Interaction | tuple[str, str, str] | str]
+Step = str | Interaction
 
-class Example(NamedTuple):
+Chain = list[Step]
+
+class Example(pydantic.BaseModel):
+    """
+    From dict:
+    >>> dictionary = {
+    ...     "prompt": "I have 2 apples, Sam gives me another 2, then I lose one, how many apples do I have?",
+    ...     "chain": [
+    ...         "Let me think...",
+    ...         { "gadget_id": "calculator", "inputs": "2+2", "outputs": "4" },
+    ...         "I have 4 apples now. I lost one.",
+    ...         { "gadget_id": "calculator", "inputs": "4-1", "outputs": "3" }
+    ...     ],
+    ...     "result": "3"
+    ... }
+    >>> example = Example(**dictionary)
+
+    To json string:
+    >>> import json
+    >>> example.json() == json.dumps(dictionary)
+    True
+    """
+
     prompt: str
     chain: Chain
     result: str
