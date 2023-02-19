d960aba061c6026dbaf039a908a9deca1554c48d
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-02-19 16:16:34 +0100

    add script for parsing gsm8k into data structures

---
+++ gsm8k.py
@@ -0,0 +1,63 @@
+import math
+import re
+
+import gadget
+import datatypes
+
+
+def parse(sample: dict[str, str]) -> datatypes.Example:
+    """
+    >>> import datasets
+    >>> dataset = datasets.load_dataset("gsm8k", "main")
+    >>> _ = parse(dataset["train"][0])
+    
+    >>> question = "I have 2 apples, Sam gives me 2 more, how many apples do I have?"
+    >>> answer = "Let me think... 2 and 2 = <<2+2=4>> 4. I have 4 apples now. #### 4"
+    >>> sample = parse({"question": question, "answer": answer})
+    >>> sample.chain
+    ['Let me think... 2 and 2 = ', Interaction(gadget_id='calculator', inputs='2+2', outputs='4'), ' 4. I have 4 apples now. ']
+    >>> sample.prompt
+    'I have 2 apples, Sam gives me 2 more, how many apples do I have?'
+    >>> sample.result
+    '4'
+
+    """
+
+    assert "answer" in sample, "answer is missing"
+    assert "question" in sample, "question is missing"
+
+    calc = gadget.Calculator()
+
+    result: str = sample["answer"]
+    chain_str, result = result.split("####")
+    result = result.strip()
+    calc_re = re.compile(r"<<(.*?)=(.*?)>>", flags=re.MULTILINE)
+
+    chain: datatypes.Chain = []
+
+    last_index = 0
+    for match in calc_re.finditer(chain_str):
+        start, end = match.span()
+        if start > last_index:
+            chain.append(chain_str[last_index:start])
+        last_index = end
+
+        gadget_input = match.group(1)
+        gadget_output_from_data = match.group(2)
+        gadget_output = calc(gadget_input)
+
+        expected = calc._float_eval(gadget_output_from_data)
+        actual = calc._float_eval(gadget_input)
+        assert math.isclose(expected, actual), f"{expected} != {actual}"
+
+        interaction = datatypes.Interaction(gadget_id="calculator", inputs=gadget_input, outputs=gadget_output)
+        chain.append(interaction)
+
+    if last_index < len(chain_str):
+        chain.append(chain_str[last_index:])
+
+    return datatypes.Example(
+        prompt=sample["question"],
+        chain=chain,
+        result=result,
+    )
