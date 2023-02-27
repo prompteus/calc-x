5fd5b89a42abec0fbc4634cd80c6c416d6733eb1
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-02-27 18:14:21 +0100

    add param whether to add result sentence in preprocessing

--- gadgets/prep.py
+++ gadgets/prep.py
@@ -4,8 +4,13 @@ import gadgets.markup
 
 
 class Preprocessing:
-    def __init__(self, tokenizer: transformers.PreTrainedTokenizer) -> None:
+    def __init__(
+        self,
+        tokenizer: transformers.PreTrainedTokenizer,
+        add_result_sentence: bool = True,
+    ) -> None:
         self.tokenizer = tokenizer
+        self.add_result_sentence = add_result_sentence
 
     def __call__(self, example: gadgets.datatypes.Example | dict) -> dict[str, list[int]]:
         if isinstance(example, dict):
@@ -13,7 +18,7 @@ class Preprocessing:
 
         soup = gadgets.markup.to_model_markup(
             example=example,
-            add_result_sentence=True,
+            add_result_sentence=self.add_result_sentence,
         )
 
         inputs = self.tokenizer(example.prompt, truncation=True)
