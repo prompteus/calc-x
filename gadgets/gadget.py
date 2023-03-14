1e1d15823166fe1e91ee4945eda1719a614c7c8c
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-03-14 11:28:48 +0100

    make some adjustments to calculator (and use _ instead of , for thousand separators to avoid some ambiguity)

--- gadgets/gadget.py
+++ gadgets/gadget.py
@@ -15,35 +15,42 @@ class Gadget(abc.ABC):
     def __call__(self, input_str: str) -> str:
         ...
 
+
 class Calculator(Gadget):
 
     @staticmethod
     def gadget_id() -> str:
         return "calculator"
 
-    def _float_eval(self, input_str: str) -> float:
-        input_str = input_str.replace(",", "")
-        expr = sympy.parsing.sympy_parser.parse_expr(input_str, evaluate=True).evalf()
+    @staticmethod
+    def _float_eval(input_str: str) -> float:
+        expr = sympy.parse_expr(input_str, evaluate=True).evalf()
         return float(expr)
 
-    def __call__(self, input_str: str) -> str:
-        try:
-            input_str = input_str.replace(",", "")
-            expr = sympy.parsing.sympy_parser.parse_expr(input_str, evaluate=True)
-            if isinstance(expr, sympy.core.numbers.Integer):
-                string = self.format_sympy_int(expr)
-            elif isinstance(expr, sympy.core.numbers.Float):
-                string = self.format_sympy_float(expr)
-            else:
-                string = f"{str(expr)} ~= {self.format_sympy_float(expr.evalf())}"
-            return string
-        except Exception as e:
-            return f"ERROR: {e}"
+    @staticmethod
+    def evaluate(input_str: str) -> sympy.Number:
+        return sympy.parse_expr(input_str, evaluate=True)
 
     @staticmethod
     def format_sympy_float(x: sympy.core.numbers.Float) -> str:
-        return f"{float(x.evalf()):,.6f}".rstrip("0").rstrip(".")
+        return f"{float(x):_.6f}".rstrip("0").rstrip(".")
 
     @staticmethod
     def format_sympy_int(x: sympy.core.numbers.Integer) -> str:
-        return f"{int(x):,}"
+        return f"{int(x):_}"
+    
+    @staticmethod
+    def format_sympy_number(x: sympy.Number) -> str:
+        if isinstance(x, sympy.core.numbers.Integer):
+            return Calculator.format_sympy_int(x)
+        elif isinstance(x, sympy.core.numbers.Float):
+            return Calculator.format_sympy_float(x)
+        else:
+            return f"{str(x)} = around {Calculator.format_sympy_float(x.evalf())}"
+    
+    def __call__(self, input_str: str) -> str:
+        try:
+            expr = self.evaluate(input_str)
+            return self.format_sympy_number(expr)
+        except Exception as e:
+            return f"ERROR: {e}"
