6006daa809d5ce1d72f73e647db95525783896c7
stefanik12 <stefanik.mich@gmail.com>
2023-04-04 18:48:34 +0200

    gsm+aqua scripts standard: default training parameters update

--- examples/train_gsm8k+aqua-slice.py
+++ examples/train_gsm8k+aqua-slice.py
@@ -80,11 +80,11 @@ def parse_and_preprocess_gsm(example: dict[str, str]):
 
 
 aqua = datasets.load_dataset("aqua_rat", split="train").map(parse_and_preprocess_aqua)
-aqua_val = datasets.load_dataset("aqua_rat", split="validation").map(parse_and_preprocess_aqua)
+aqua_val = datasets.load_dataset("aqua_rat", split="validation").map(parse_and_preprocess_aqua).select(range(100))
 
 gsm8k = datasets.load_dataset("gsm8k", "main").map(parse_and_preprocess_gsm)
 
-train_valid_ds = gsm8k["train"].train_test_split(test_size=400, seed=42)
+train_valid_ds = gsm8k["train"].train_test_split(test_size=100, seed=42)
 
 # upscaling GSM - we don't do that now
 # gsm_idx = list(range(len(train_valid_ds["train"])))
@@ -181,22 +181,22 @@ training_args = transformers.Seq2SeqTrainingArguments(
     learning_rate=5e-5,
     do_train=True,
     do_eval=True,
-    warmup_steps=1000,
-    max_steps=1_000_000,
-    per_device_train_batch_size=32,
-    gradient_accumulation_steps=1,
+    warmup_steps=5000,
+    max_steps=50_000,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=10,
     per_device_eval_batch_size=1,
     eval_accumulation_steps=16,
-    logging_steps=50,
-    eval_steps=4000,
-    save_steps=4000,
+    logging_steps=100,
+    eval_steps=2000,
+    save_steps=2000,
     evaluation_strategy="steps",
     bf16=True,
     predict_with_generate=True,
     generation_max_length=512,
     include_inputs_for_metrics=True,
     report_to="wandb",
-    metric_for_best_model="correct_results",
+    metric_for_best_model="aqua_correct_results",
     greater_is_better=True,
     load_best_model_at_end=True,
     save_total_limit=3,
