d5a796793bbd1bf656a34d88e76b00932c12855a
Marek Kadlcik <10684818+markcheeky@users.noreply.github.com>
2023-05-23 21:53:19 +0200

    make some adjustments to the calc formatting

--- gadgets/gadget.py
+++ gadgets/gadget.py
@@ -26,8 +26,8 @@ class Calculator(Gadget):
 
     @staticmethod
     def _float_eval(input_str: str) -> float:
-        expr = sympy.parse_expr(input_str, evaluate=True).evalf()
-        return float(expr)
+        expr = Calculator.evaluate(input_str)
+        return float(expr.evalf())
 
     @staticmethod
     def evaluate(input_str: str) -> sympy.Number:
@@ -42,17 +42,28 @@ class Calculator(Gadget):
         return f"{int(x):_}"
     
     @staticmethod
-    def format_sympy_number(x: sympy.Number) -> str:
+    def format_sympy_number(x: sympy.Number, add_approx: bool = True) -> str:
         if isinstance(x, sympy.core.numbers.Integer):
             return Calculator.format_sympy_int(x)
-        elif isinstance(x, sympy.core.numbers.Float):
+        if isinstance(x, sympy.core.numbers.Float):
             return Calculator.format_sympy_float(x)
-        else:
+        if isinstance(x, sympy.core.numbers.Rational):
+            num, den = x.as_numer_denom()
+            num = Calculator.format_sympy_number(num, add_approx=False)
+            den = Calculator.format_sympy_number(den, add_approx=False)
+            string = f"{num}/{den}"
+            if add_approx:
+                string += f" = around {Calculator.format_sympy_float(x.evalf())}"
+            return string
+        
+        if add_approx:
             return f"{str(x)} = around {Calculator.format_sympy_float(x.evalf())}"
+        
+        return str(x)
     
-    def __call__(self, input_str: str) -> str:
+    def __call__(self, input_str: str, add_approx: bool = True) -> str:
         try:
             expr = self.evaluate(input_str)
-            return self.format_sympy_number(expr)
+            return self.format_sympy_number(expr, add_approx=add_approx)
         except Exception as e:
             return f"ERROR: {e}"
