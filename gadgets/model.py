69bdef6bd49abb96f2a3928fd158ddc43d93545b
stefanik12 <stefanik.mich@gmail.com>
2024-01-10 17:00:43 +0100

    Generate: output interval delimiter

--- gadgets/model.py
+++ gadgets/model.py
@@ -272,7 +272,7 @@ class StepwiseGenerator(T5ForConditionalGeneration, GadgetAssist):
             return torch.rand((1, 0))
 
         generated_output_ids = torch.hstack([extended_input_ids[:, :-1], output_ids])
-        generated_output_ids = generated_output_ids[:, input_ids.shape[-1] + 1:]  # we assume batch_size==1 here
+        generated_output_ids = generated_output_ids[:, input_ids.shape[-1] - 1:]  # we assume batch_size==1 here
 
         return generated_output_ids
 
